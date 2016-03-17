e4ab899c5fe2504ce025b57543845d2e64b51e57
Wang Huizhou <whz.job@gmail.com>
2016-03-17 15:06:04 +0800

    Feature: add dead lock

--- ThreadPool/src/com/backaway/threadpool/rejectedExecutionHandler/MyRunnable.java
+++ ThreadPool/src/com/backaway/threadpool/rejectedExecutionHandler/MyRunnable.java
@@ -10,6 +10,7 @@ class MyRunnable implements Runnable {
 	@Override
 	public void run() {
 		try {
+			System.out.println(Thread.currentThread().getName() + "正在执行。。。");
 			System.out.println(this.name + " is running.");
 			Thread.sleep(100);
 		} catch (Exception e) {
