b82ebb3af6b7ab92950399aa207606c975209783
Wang Huizhou <whz.job@gmail.com>
2016-05-23 11:11:25 +0800

    Add: rxjava

--- Framework/Kafka-demo/src/com/backaway/ConsumerDemo.java
+++ Framework/Kafka-demo/src/com/backaway/ConsumerDemo.java
@@ -1,16 +1,17 @@
 package com.backaway;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
- 
+
 import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
- 
+
 /**
  * 详细可以参考：https://cwiki.apache.org/confluence/display/KAFKA/Consumer+Group+Example
  * 
@@ -21,29 +22,42 @@ public class ConsumerDemo {
     private final ConsumerConnector consumer;
     private final String topic;
     private ExecutorService executor;
- 
-    public ConsumerDemo(String a_zookeeper, String a_groupId, String a_topic) {
-        consumer = Consumer.createJavaConsumerConnector(createConsumerConfig(a_zookeeper,a_groupId));
-        this.topic = a_topic;
+
+    /**
+     * @param zookeeper
+     * @param groupId
+     * @param topic
+     */
+    public ConsumerDemo(String zookeeper, String groupId, String topic) {
+        consumer = Consumer.createJavaConsumerConnector(createConsumerConfig(zookeeper, groupId));
+        this.topic = topic;
     }
- 
+
+    /**
+     * 
+     */
     public void shutdown() {
-        if (consumer != null)
+        if (consumer != null) {
             consumer.shutdown();
-        if (executor != null)
+        }
+        if (executor != null) {
             executor.shutdown();
+        }
     }
- 
+
+    /**
+     * @param numThreads
+     */
     public void run(int numThreads) {
         Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
         topicCountMap.put(topic, new Integer(numThreads));
         Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer
                 .createMessageStreams(topicCountMap);
         List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
- 
+
         // now launch all the threads
         executor = Executors.newFixedThreadPool(numThreads);
- 
+
         // now create an object to consume the messages
         //
         int threadNumber = 0;
@@ -52,33 +66,32 @@ public class ConsumerDemo {
             threadNumber++;
         }
     }
- 
-    private static ConsumerConfig createConsumerConfig(String a_zookeeper,
-            String a_groupId) {
+
+    private static ConsumerConfig createConsumerConfig(String zookeeper, String groupId) {
         Properties props = new Properties();
-        props.put("zookeeper.connect", a_zookeeper);
-        props.put("group.id", a_groupId);
+        props.put("zookeeper.connect", zookeeper);
+        props.put("group.id", groupId);
         props.put("zookeeper.session.timeout.ms", "400");
         props.put("zookeeper.sync.time.ms", "200");
         props.put("auto.commit.interval.ms", "1000");
- 
+
         return new ConsumerConfig(props);
     }
- 
+
     public static void main(String[] arg) {
         String[] args = { "10.211.55.13:2181", "group-1", "log_collecter", "12" };
         String zooKeeper = args[0];
         String groupId = args[1];
         String topic = args[2];
         int threads = Integer.parseInt(args[3]);
- 
+
         ConsumerDemo demo = new ConsumerDemo(zooKeeper, groupId, topic);
         demo.run(threads);
- 
+
         try {
             Thread.sleep(10000);
         } catch (InterruptedException ie) {
- 
+
         }
         demo.shutdown();
     }
--- Framework/Kafka-demo/src/com/backaway/ConsumerMsgTask.java
+++ Framework/Kafka-demo/src/com/backaway/ConsumerMsgTask.java
@@ -2,21 +2,26 @@ package com.backaway;
 
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
- 
+
+/**
+ * @author Backaway
+ *
+ */
 public class ConsumerMsgTask implements Runnable {
-    private KafkaStream m_stream;
-    private int m_threadNumber;
- 
+    private KafkaStream stream;
+    private int threadNumber;
+
     public ConsumerMsgTask(KafkaStream stream, int threadNumber) {
-        m_threadNumber = threadNumber;
-        m_stream = stream;
+        this.threadNumber = threadNumber;
+        this.stream = stream;
     }
- 
+
+    @Override
     public void run() {
-        ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
-        while (it.hasNext())
-            System.out.println("Thread " + m_threadNumber + ": "
-                    + new String(it.next().message()));
-        System.out.println("Shutting down Thread: " + m_threadNumber);
+        ConsumerIterator<byte[], byte[]> it = stream.iterator();
+        while (it.hasNext()) {
+            System.out.println("Thread " + threadNumber + ": " + new String(it.next().message()));
+        }
+        System.out.println("Shutting down Thread: " + threadNumber);
     }
 }
